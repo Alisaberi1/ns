293eceb5ba9d3b9ad6bd74bdcac1c37118378a58
Ali $@ <90723218+Alisaberi1@users.noreply.github.com>
2022-04-02 19:18:18 +0430

    Update waterdegree.py

--- waterdegree.py
+++ waterdegree.py
@@ -1,7 +1,9 @@
 T=int(input())
-if T>100:
-  print("Steam")
-if T<0:
-  print("Ice")
-else:
-  print("Water")
+while -273<=T<6000:
+  if T>100:
+    print("Steam")
+  if T<0:
+    print("Ice")
+  if not T<0 and not T>100:
+    print("Water")
+  break
